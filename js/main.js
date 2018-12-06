aaec9caba65f533f11bf4bb11616f1e275c16797
Makhova <makhova.maria@gmail.com>
2018-12-06 17:49:00 +0300

    Сделана активация карты

--- js/main.js
+++ js/main.js
@@ -7,12 +7,19 @@ var accomodationType = ['palace', 'flat', 'house', 'bungalo'];
 var LOCATION_MIN_Y = 130;
 var LOCATION_MAX_Y = 630;
 var cards = [];
-var mapPin = document.querySelector('.map__pin');
-var mapPinWidth = window.getComputedStyle(mapPin).width;
-var mapPinHeight = window.getComputedStyle(mapPin).height;
+var pin = document.querySelector('.map__pin');
+var pinWidth = window.getComputedStyle(pin).width;
+var pinHeight = window.getComputedStyle(pin).height;
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinsList = document.querySelector('.map__pins');
 var advertsNumber = 8;
+var form = document.querySelector('.ad-form');
+var fieldset = form.querySelectorAll('fieldset');
+var filters = document.querySelector('.map__filters');
+var mainPin = document.querySelector('.map__pin--main');
+var mainPinWidth = window.getComputedStyle(mainPin).width;
+var mainPinHeight = window.getComputedStyle(mainPin).height;
+var addressField = document.getElementById('address');
 
 var getLinkAvatar = function (index) {
   var avatar = 'img/avatars/user' + 0 + (index + 1) + '.png';
@@ -27,7 +34,7 @@ var getRandomNumber = function (min, max) {
 };
 
 var getLocationMinX = function () {
-  var locationMinX = 0.5 * parseInt(mapPinWidth, 10);
+  var locationMinX = 0.5 * parseInt(pinWidth, 10);
 
   return locationMinX;
 };
@@ -46,7 +53,7 @@ var createCard = function (array, index) {
     },
 
     offer: {
-      type: array[index],
+      type: array[Math.floor(index / 2)],
     },
 
     location: {
@@ -59,14 +66,46 @@ var createCard = function (array, index) {
 };
 
 var addPinElement = function (advert) {
-  var pin = pinTemplate.cloneNode(true);
-  pin.style.left = advert.location.x - getLocationMinX() + 'px';
-  pin.style.top = advert.location.y - parseInt(mapPinHeight, 10) + 'px';
-  pin.querySelector('img').src = advert.author.avatar;
-  pin.querySelector('img').alt = 'Заголовок объявления';
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.left = advert.location.x - getLocationMinX() + 'px';
+  pinElement.style.top = advert.location.y - parseInt(pinHeight, 10) + 'px';
+  pinElement.querySelector('img').src = advert.author.avatar;
+  pinElement.querySelector('img').alt = 'Заголовок объявления';
 
-  return pin;
+  return pinElement;
 };
+
+var getMainPinX = function () {
+  var mainPinX = Math.round(parseInt(mainPin.style.left, 10) + 0.5 * parseInt(mainPinWidth, 10));
+
+  return mainPinX;
+};
+var getMainPinCentreY = function () {
+  var mainPinCentreY = Math.round(parseInt(mainPin.style.top, 10) + 0.5 * parseInt(mainPinHeight, 10));
+
+  return mainPinCentreY;
+};
+
+var getMainPinY = function () {
+  var mainPinY = Math.round(parseInt(mainPin.style.top, 10) + parseInt(mainPinHeight, 10));
+
+  return mainPinY;
+};
+
+var buttonClickHandler = function () {
+  map.classList.remove('map--faded');
+  filters.classList.remove('map__filters--disabled');
+  form.classList.remove('ad-form--disabled');
+  fieldset.disabled = false;
+  addAdvertPin(advertsNumber);
+  addressField.value = getMainPinX() + ', ' + getMainPinY();
+};
+
+filters.classList.add('map__filters--disabled');
+fieldset.disabled = true;
+addressField.value = getMainPinX() + ', ' + getMainPinCentreY();
+mainPin.addEventListener('click', buttonClickHandler);
+mainPin.addEventListener('keypress', buttonClickHandler);
 
 var addAdvertPin = function (advertsCounter) {
   for (var i = 0; i < advertsCounter; i++) {
@@ -78,8 +117,3 @@ var addAdvertPin = function (advertsCounter) {
 
   return advertPin;
 };
-
-map.classList.remove('map--faded');
-addAdvertPin(advertsNumber);
-
-
