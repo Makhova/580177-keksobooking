a6ac43fea2aa7486d7182cfd9a4cfccfdc3dc423
Makhova <makhova.maria@gmail.com>
2019-01-18 12:28:01 +0300

    Добавлен вызов функции window.load

--- js/load.js
+++ js/load.js
@@ -1,16 +1,17 @@
 'use strict';
 
 (function () {
-  window.load = function (callback) {
+  window.load = function () {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
       console.log(xhr.response);
-      callback(xhr.response);
     });
 
     xhr.open('GET', 'https://js.dump.academy/keksobooking/data');
     xhr.send();
   };
+
+  window.load();
 })();
